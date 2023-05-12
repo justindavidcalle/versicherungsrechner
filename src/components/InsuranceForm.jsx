81d79665f1d54f0e9ef813514e317ed10157c21c
justindavidcalle <justincalle@outlook.de>
2023-05-12 02:10:51 +0200

    Totalberechnung fertig
    Neue Komponente und CSS Design für Main Seite

--- src/components/InsuranceForm.jsx
+++ src/components/InsuranceForm.jsx
@@ -16,32 +16,32 @@ export function InsuranceForm(props) {
     }
 
     return(
-        <>
-        <h1>Versicherungsrechner</h1>
-            <form onSubmit={makelist}>
-                <h2>Versicherungsname</h2>
-                <input 
-                value={insuranceName} 
-                onChange={e => setInsuranceName(e.target.value)} 
-                type="text" 
-                placeholder="Name der Firma"/>
-                <h2>Preis</h2>
-                <input 
-                value={insurancePrice}
-                onChange={e => setInsurancePrice(e.target.value)}
-                type="number" 
-                placeholder="Beträge die sie bezahlen" 
-                />
-                <h2>Monatlich/Jährlich</h2>
-                <label>
-                    <select value={paymentOption} onChange={e =>setPaymentOption(e.target.value)}>
-                        <option>Monatlich</option>
-                        <option>Jährlich</option>
-                    </select>
-                </label>
+        <div className="formOfInsurance">
+            <h1>Versicherungsrechner</h1>
+                <form onSubmit={makelist}>
+                    <h2>Versicherungsname</h2>
+                    <input 
+                    value={insuranceName} 
+                    onChange={e => setInsuranceName(e.target.value)} 
+                    type="text" 
+                    placeholder="Name der Firma"/>
+                    <h2>Preis</h2>
+                    <input 
+                    value={insurancePrice}
+                    onChange={e => setInsurancePrice(e.target.value)}
+                    type="number" 
+                    placeholder="Beträge die sie bezahlen" 
+                    />
+                    <h2>Monatlich/Jährlich</h2>
+                    <label>
+                        <select value={paymentOption} onChange={e =>setPaymentOption(e.target.value)}>
+                            <option>Monatlich</option>
+                            <option>Jährlich</option>
+                        </select>
+                    </label>
 
-                <button>hinzufügen</button>
-            </form>
-            </>
+                    <button>hinzufügen</button>
+                </form>
+            </div>
     )
 }
